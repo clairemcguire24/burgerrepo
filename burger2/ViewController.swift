fdf7f2822bc49063ed8de0e8cf5c0224c173f341
Claire McGuire <claireviolet05@icloud.com>
2023-12-05 09:35:21 -0600

    Initial Commit

---
+++ burger2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  burger2
+//
+//  Created by CLAIRE MCGUIRE on 12/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
